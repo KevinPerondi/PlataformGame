22e5420ac21a88c4fdf5f95b16d625b735bf40fc
kevinperondi <kevinpregis@hotmail.com>
2018-06-20 03:25:19 -0300

    praticando ideias

---
+++ src/BootState.js
@@ -0,0 +1,34 @@
+'use strict'
+
+class BootState extends Phaser.State {
+
+    preload() {
+        this.game.load.image('background1','assets/background1.jpeg')
+        this.game.load.image('sky', 'assets/sky.png')
+        this.game.load.image('plane1', 'assets/airplane1.png')
+        this.game.load.image('shot', 'assets/shot.png')
+        this.game.load.image('wall', 'assets/wall.png')
+        this.game.load.image('fog', 'assets/fog.png')
+        this.game.load.image('rain', 'assets/rain.png')
+        this.game.load.image('saw', 'assets/saw.png')
+        this.game.load.image('smoke', 'assets/smoke.png')
+        this.game.load.image('title', 'assets/title.png')
+
+        this.game.load.spritesheet('explosion', 'assets/explosion.png', 56, 56)
+
+        // map
+        /*his.game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.tilemap('level2', 'assets/level2.json', null, Phaser.Tilemap.TILED_JSON)
+        this.game.load.image('tiles1','assets/tileset-42x42.png');*/
+
+        this.game.load.image('groundTiles','assets/groundTiles.png');
+        this.game.load.image('background','assets/background.png');
+        this.game.load.tilemap('fase1', 'assets/fase1.json', null, Phaser.Tilemap.TILED_JSON)
+    }
+
+    create() {
+        console.log("BootState created")
+        // this.state.start('Title')
+        this.state.start('Game')
+    }
+}
